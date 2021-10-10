4bc7b2cccdcf78d011bc39c4307bcc6d2a5267e9
Steven Loaiza <sloaiza@geico.com>
2021-10-10 15:00:45 -0400

    Answer last digit of sum of sq fib

--- Algorithmic-Toolbox/Algorithmic Warm Up/Last Digit of the Sum of Squares of Fibonacci Numbers/last_digit_of_the_sum_of_squares_of_fibonacci_numbers.py
+++ Algorithmic-Toolbox/Algorithmic Warm Up/Last Digit of the Sum of Squares of Fibonacci Numbers/last_digit_of_the_sum_of_squares_of_fibonacci_numbers.py
@@ -15,11 +15,31 @@ def last_digit_of_the_sum_of_squares_of_fibonacci_numbers_naive(n):
 
     return sum([f ** 2 for f in fibonacci_numbers]) % 10
 
+def check_repeat(n):
+    """Check if sequence repeats
+    Hint: Try n=119, repeats after 60.
+    """
+    assert (n+1) % 2 == 0
+    return_ls = list(range(n+1))
+    for i in range(n+1):
+        return_ls[i] = last_digit_of_the_sum_of_squares_of_fibonacci_numbers_naive(i)
+
+    idx = int((n+1)/2)
+    if return_ls[:idx] != return_ls[idx:]:
+        print('Sequence does not repeat')
+    else:
+        print('Sequence repeats at n = %s' % (n+1/2))
 
 def last_digit_of_the_sum_of_squares_of_fibonacci_numbers(n):
     assert 0 <= n <= 10 ** 18
 
-    type here
+    n = n % 60
+    prev, curr = 0, 1
+
+    for i in range(2, n+1):
+        prev, curr = curr, prev + curr
+
+    return (curr * (prev + curr)) % 10
 
 
 if __name__ == '__main__':
