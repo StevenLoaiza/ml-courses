6dcea8ea2d861566a934ef55bf4d01a700df6f60
Steven Loaiza <sloaiza@geico.com>
2021-11-28 22:53:15 -0500

    Modify car fueling scirpt

--- Algorithmic-Toolbox/Greedy Algorithms/Car Fueling/car_fueling.py
+++ Algorithmic-Toolbox/Greedy Algorithms/Car Fueling/car_fueling.py
@@ -7,7 +7,18 @@ def compute_min_number_of_refills(d, m, stops):
     assert 1 <= len(stops) <= 300
     assert 0 < stops[0] and all(stops[i] < stops[i + 1] for i in range(len(stops) - 1)) and stops[-1] < d
 
-    type here
+    stops = [0] + stops + [d]
+    num_refills, cur_refill = 0, 0
+    while cur_refill <= len(stops) - 2:
+        last_refill = cur_refill
+        while cur_refill <= len(stops) - 2 and stops[cur_refill + 1] - stops[last_refill] <= m:
+            cur_refill += 1
+        if cur_refill == last_refill:
+            return -1
+        if cur_refill <= len(stops) - 2:
+            num_refills += 1
+
+    return num_refills
 
 
 if __name__ == '__main__':
