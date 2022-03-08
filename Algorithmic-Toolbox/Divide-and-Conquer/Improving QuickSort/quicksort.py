4aca97fca18539d36326b3b3237d4394dc0f6305
Steven Loaiza <46308439+StevenLoaiza@users.noreply.github.com>
2022-03-07 22:19:18 -0500

    Update quicksort with 3-way partition
    All test passed

--- Algorithmic-Toolbox/Divide-and-Conquer/Improving QuickSort/quicksort.py
+++ Algorithmic-Toolbox/Divide-and-Conquer/Improving QuickSort/quicksort.py
@@ -4,7 +4,30 @@ from random import randint
 
 
 def partition3(array, left, right):
-    type here
+    """
+    Paritions the array into two section, left side is less than the pivot, right side is greater than the pivot.
+    It ensures that we avoid a recursive call when there are multiple values for the pivot (no need to sort them).
+    :param array: 
+    :param left: 
+    :param right: 
+    :return: 
+    """
+    x = array[left]  # Pivot
+    j = left
+    for i in range(left + 1, right + 1):
+        if array[i] < x:
+            j = j + 1  # increment position j
+            array[j], array[i] = array[i], array[j]
+    array[j], array[left] = array[left], array[j]
+
+    x = array[j]  # Pivot
+    jj = j
+    for i in range(jj + 1, right + 1):
+        if array[i] == x:
+            jj = jj + 1  # increment position j
+            array[jj], array[i] = array[i], array[jj]
+
+    return j, jj
 
 
 def randomized_quick_sort(array, left, right):
@@ -12,8 +35,9 @@ def randomized_quick_sort(array, left, right):
         return
     k = randint(left, right)
     array[left], array[k] = array[k], array[left]
-    make a call to partition3 and then two recursive calls 
-to randomized_quick_sort
+    m1, m2 = partition3(array, left, right)
+    randomized_quick_sort(array, left, m1)
+    randomized_quick_sort(array, m2 + 1, right)
 
 
 if __name__ == '__main__':
